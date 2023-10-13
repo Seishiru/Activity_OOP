3f905dc50e7f761264c5aebaca020be33f1d6723
Kotegawa <143763801+Seishiru@users.noreply.github.com>
2023-10-13 09:03:10 +0800

    Add files via upload

---
+++ shoppe/src/shoppe/Calculator.java
@@ -0,0 +1,17 @@
+package shoppe;
+
+public class Calculator {
+    public int add(int x, int y){
+        return x + y;
+    }
+    
+    // This method could throw an ArithmeticException
+    public double divide(int x, int y) {
+        try{
+            int z = x / y;
+        }catch(ArithmeticException e){
+            System.err.println("Error: Division by zero is not allowed." + e);
+        }
+        return x / y;
+    }
+}
---
+++ shoppe/src/shoppe/ShoppingCart.java
@@ -0,0 +1,18 @@
+package shoppe;
+
+public class ShoppingCart {
+
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+        int addResult = calc.add(43, 79);
+        System.out.println("Add Result: " + addResult);
+
+        
+        try{
+            double divResult = calc.divide(15, 0);
+            System.out.println("Division Result: " + divResult);
+        }catch(ArithmeticException e){
+            System.err.println("Error: Division by zero is not allowed." + e);
+        }
+    }
+}
